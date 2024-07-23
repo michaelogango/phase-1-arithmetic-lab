5c6babca01ba1f26baf0a8197d80ad531b062279
Michael Odhiambo <michaelogango5@gmail.com>
2024-07-23 17:47:54 +0300

    work done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1=31
+let num2=2
 
-// Write your code here
+let random=23;
+
+let multiply=num1*num2
+
+let num3 = 14;
+let num4 = 5;  
+
+
+let mod = num3 % num4;
+
+let numbers = [3, 5, 7, 10, 20, 15];
+
+
+let max = Math.max(...numbers);
